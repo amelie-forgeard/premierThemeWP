3c370dc200f5693f4dd455b8bdd987d52b7dad0e
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-09-19 17:11:51 +0200

    commentaires des différentes fct de départ

--- header.php
+++ header.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
+<!-- définir automatiquement la langue sur le réglage WP -->
 <html <?php language_attributes(); ?>>
 <head>
+    <!-- fonction bloginfo = utf8 -->
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    
+    <!-- fonction essentielle au bon fonctionnement de mon thème wp_head() -->
     <?php wp_head(); ?>
 </head>
-
+    <!-- fct pour obtenir des noms de classes css en fct de la page visitée body_class() -->
 <body <?php body_class(); ?>>
-    
-    <?php wp_body_open(); ?>
+<!-- a fonction wp_body_open() a été ajoutée dans WordPress 5.2 afin de permettre à des extensions d’écrire du code au début du body. C’est utile notamment pour Yoast qui vient y placer le Google Tag Manager et autres codes de scripts.    
+<?php wp_body_open(); ?> -->
