076dd3ec5fe5f939f15949805ab2927dbf9ea3e0
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-09-19 15:15:57 +0200

    mise en place theme avec index.php

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    
+    <?php wp_head(); ?>
 </head>
-<body>
-    <h1>allo</h1>
-</body>
-</html>
+
+<body <?php body_class(); ?>>
+    
+    <?php wp_body_open(); ?>
