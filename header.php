715f31909e69b6a1db40e0125980bbc80f0f6412
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-09-20 12:14:39 +0200

    boucle afficahge des articles du blog

--- header.php
+++ header.php
@@ -9,10 +9,13 @@
     <!-- wp_head() fct qui récupère les scripts et les styles : -->
     <?php wp_head(); ?>
 </head>
-    <!-- fct pour obtenir des noms de classes css en fct de la page visitée body_class() -->
+    <!-- fct pour obtenir des noms de classes css en fct de la 
+    page visitée body_class() grace au template hierarchy -->
 <body <?php body_class(); ?>>
 <header class="header">
+    <!-- qd je clique sur le logo ça me renvoie à l'accueil home_url() -->
     <a href="<?php echo home_url( '/' ); ?>">
+    <!-- fct get_template_directory_uri() afin d’obtenir l’adresse absolue  -->
       <img src="<?php echo get_template_directory_uri(); ?>/img/logo.jpg" alt="Logo">
     </a>  
   </header>
--- page.php
+++ page.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    <!-- je suis à l'intérieur de la boucle, 
+    j'utilise les templates tags the_title et the_content -->
+    	<h1><?php the_title(); ?></h1>
     
+    	<?php the_content(); ?>
 
 	<?php endwhile; endif; ?>
 
