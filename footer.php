b82b7aac518acf6c4ba19d5526fd6fdd1cea21a7
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-09-20 15:56:48 +0200

    creation dossier parts pour tous les templates parts crées

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <!-- appel de la fct template part newsletter ne pas mettre l'extension .php à la fin -->
-<?php get_template_part( 'newsletter' ); ?>
+<?php get_template_part( 'parts/newsletter' ); ?>
 
 <?php wp_footer(); ?>
