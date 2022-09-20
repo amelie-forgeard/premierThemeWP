986db0a18098c58e5d79505b8cb923e1769745ea
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-09-20 18:39:12 +0200

    expl conditional tags pour afficher un titre

--- archive.php
+++ archive.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
  	<h1>Le premier blog WP d'Amélie</h1>
+     <?php 
+    if ( is_category() ) {
+        $title = "Catégorie : " . single_tag_title( '', false );
+    }
+    elseif ( is_tag() ) {
+        $title = "Étiquette : " . single_tag_title( '', false );
+    }
+    elseif ( is_search() ) {
+        $title = "Vous avez recherché : " . get_search_query();
+    }
+    else {
+        $title = 'Le Blog';
+    }
+?>
+<h1><?php echo $title; ?></h1>
 
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
   
