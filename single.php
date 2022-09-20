185a43d651e5dad844ced55c9ebda11ceca03aa7
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-09-20 15:45:30 +0200

    affiche d'un article, détail de la boucle

--- single.php
+++ single.php
@@ -1,3 +1,28 @@
 <?php get_header(); ?>
-	<h1>SINGLE</h1>
+  <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    
+    <article class="post">
+      <?php the_post_thumbnail(); ?>
+
+      <h1><?php the_title(); ?></h1>
+
+      <div class="post__meta">
+        <!-- je reécupère et j'affiche l'avatar vace get_avatar
+        je récupère son identifiant et 40 = taille de l'image -->
+        <?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
+        <p>
+          Publié le <?php the_date(); ?>
+          par <?php the_author(); ?>
+          Dans la catégorie <?php the_category(); ?>
+          Avec les étiquettes <?php the_tags(); ?>
+        </p>
+      </div>
+
+      <div class="post__content">
+        <!-- affiche le contenu écrit de puis l'éditeur Gutenberg ou autre -->
+        <?php the_content(); ?>
+      </div>
+    </article>
+
+  <?php endwhile; endif; ?>
 <?php get_footer(); ?>
