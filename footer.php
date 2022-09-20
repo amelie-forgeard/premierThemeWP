42b65066de9693f834a2d9be0ecd6db4de1837ed
Amelie FORGEARD <amelieforgeard@gmail.com>
2022-09-20 15:55:00 +0200

    appel template part newletter ds footer

--- footer.php
+++ footer.php
@@ -1,1 +1,4 @@
+<!-- appel de la fct template part newsletter ne pas mettre l'extension .php à la fin -->
+<?php get_template_part( 'newsletter' ); ?>
+
 <?php wp_footer(); ?>
--- home.php
+++ home.php
@@ -4,18 +4,28 @@
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
   
 		<article class="post">
+            <!-- affiche le titre article: -->
 			<h2><?php the_title(); ?></h2>
-      
+            <!-- affiche l'image de mise en avant: -->
+            <!-- Pour rappel on avait activé cette fonctionnalité grâce à la 
+            fonction add_theme_support('post-thumbnails') 
+            dans le fichier functions.php, -->
         	<?php the_post_thumbnail(); ?>
             
             <p class="post__meta">
+                <!-- affiche date de publi et get_option récupère 
+                le format de date choisi dans les réglages WP -->
                 Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
+                <!-- affiche aueur + nb commentaires -->
                 par <?php the_author(); ?> • <?php comments_number(); ?>
             </p>
-            
+            <!-- affiche un extrait: -->
+            <!-- attention de ne pas l'entourer de balise paragraphe ! -->
       		<?php the_excerpt(); ?>
               
       		<p>
+                <!-- affiche le lien vers l'article, le mettre dans une balise a 
+            dans l'attribut href-->
                 <a href="<?php the_permalink(); ?>" class="post__link">Lire la suite</a>
             </p>
 		</article>
